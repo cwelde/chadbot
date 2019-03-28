4eca900737f74eba96b925610ad8a49a8359b32e
Christopher <cwelde@uci.edu>
2019-03-28 00:20:27 -0700

    Added function to tweet

--- tweet.py
+++ tweet.py
@@ -1,6 +1,21 @@
 import tweepy
 
+consumer_key =""
+consumer_secret =""
+access_token =""
+access_token_secret =""
+
+def sendTweet(ck, cs, at, ats, message):
+    
+    auth = tweepy.OAuthHandler(ck,cs)
+
+    auth.set_access_token(at,ats)
+
+    api = tweepy.API(auth)
+
+    api.update_status(status = message)
 
 
 if __name__ == "__main__":
-    print("Hello")
+
+    sendTweet(consumer_key,consumer_secret,access_token,access_token_secret, "Your Message Here")
