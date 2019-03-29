c8683994d212b2521bb04829ec0322d6a5d85459
Christopher <cwelde@uci.edu>
2019-03-28 23:02:44 -0700

    generates sentences using markov chains and presidents old speechs. Randomly tweets these sentences every 2 minutes

--- tweet.py
+++ tweet.py
@@ -1,10 +1,21 @@
 import tweepy
+import numpy as np
+import time
 
-consumer_key =""
-consumer_secret =""
-access_token =""
-access_token_secret =""
+tr = open('speeches.txt', encoding = 'utf-8').read()
+corpus = tr.split()
 
+consumer_key ="YJ8loqlt08YF3ydEhNBw8NEE5"
+consumer_secret ="oOttd5uNKODF7kW77qhnsB8KU70360p4JNLZMxwpvX3MKhNzml"
+access_token ="1110649385635868673-cbMHENGmpccqXebb9vANZuF6LRFaXV"
+access_token_secret ="Y1evBVfONJTYDSQyFZwxrGeS4M682OyyEtBWy2peBgvKp"
+
+def makePairs(corpus):
+    for i in range(len(corpus) - 1):
+        yield (corpus[i], corpus[i + 1])
+
+
+    
 def sendTweet(ck, cs, at, ats, message):
     
     auth = tweepy.OAuthHandler(ck,cs)
@@ -17,5 +28,25 @@ def sendTweet(ck, cs, at, ats, message):
 
 
 if __name__ == "__main__":
+    while True:
+        pairs = makePairs(corpus)
 
-    sendTweet(consumer_key,consumer_secret,access_token,access_token_secret, "Your Message Here")
+        word_dict = {}
+
+        for word_1, word_2 in pairs:
+            if word_1 in word_dict.keys():
+                word_dict[word_1].append(word_2)
+            else:
+                word_dict[word_1] = [word_2]
+
+        first_word = np.random.choice(corpus)
+        chain = [first_word]
+        n_words = 30
+
+        for i in range(n_words):
+            chain.append(np.random.choice(word_dict[chain[-1]]))
+
+        myMessage = ' '.join(chain)
+        sendTweet(consumer_key,consumer_secret,access_token,access_token_secret, myMessage)
+        print("Just sent one")
+        time.sleep(120)
